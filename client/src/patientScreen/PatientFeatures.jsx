5eb31c5e6401eab0b2a07e04c7d3c674140bba04
Aakash Kumar Chaudhary <aakaas.c.4508@gmail.com>
2023-07-04 09:52:52 +0530

    added patient features page

--- client/src/patientScreen/PatientFeatures.jsx
+++ client/src/patientScreen/PatientFeatures.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
-
+import '../patientStyles/patientFeatures.css'
+import consultant from '../patientComponentsAssets/consultant.jpg'
 const PatientFeatures = () => {
   return (
-    <div>PatientFeatures</div>
+    <div className="body">
+        <div className='features_cards'>
+            <div className="all_cards">
+                <div className="card">
+                    <img src={consultant} alt="" />
+                    <div className="offer">
+                        <h6>40% off</h6>
+                    </div>
+                    <h3>Get your consultant TODAY</h3>
+                </div>
+                <div className="card">
+                    <img src={consultant} alt="" />
+                    <div className="offer">
+                        <h6>40% off</h6>
+                    </div>
+                    <h3>Get your consultant TODAY</h3>
+                </div>
+                <div className="card">
+                    <img src={consultant} alt="" />
+                    <div className="offer">
+                        <h6>40% off</h6>
+                    </div>
+                    <h3>Get your consultant TODAY</h3>
+                </div>
+                <div className="card">
+                    <img src={consultant} alt="" />
+                    <div className="offer">
+                        <h6>40% off</h6>
+                    </div>
+                    <h3>Get your consultant TODAY</h3>
+                </div>
+                <div className="card">
+                    <img src={consultant} alt="" />
+                    <div className="offer">
+                        <h6>40% off</h6>
+                    </div>
+                    <h3>Get your consultant TODAY</h3>
+                </div>
+                <div className="card">
+                    <img src={consultant} alt="" />
+                    <div className="offer">
+                        <h6>40% off</h6>
+                    </div>
+                    <h3>Get your consultant TODAY</h3>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  
   )
 }
+
 
 export default PatientFeatures
