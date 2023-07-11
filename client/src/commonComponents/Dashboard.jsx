f86df4f54b9869c3020a87873d3154ffe1f642a3
Aakash Kumar Chaudhary <aakaas.c.4508@gmail.com>
2023-07-11 11:13:55 +0530

    changed dashboard layout

--- client/src/commonComponents/Dashboard.jsx
+++ client/src/commonComponents/Dashboard.jsx
@@ -5,7 +5,7 @@ import { Link,useNavigate} from "react-router-dom";
 const Dashboard = () => {
   return (
     <div className='common-main'>
-        <img src={dashboard} className='background-image'></img>
+        {/* <img src={dashboard} className='background-image'></img>
         <div className="adminBtn">
           <button><Link to='/adminHome'>Admin Login</Link></button> 
 
@@ -13,6 +13,17 @@ const Dashboard = () => {
         <div className="common-overlay-button">
             <button><Link to='/patientHome'>Are You Patient? Click Here</Link></button>
             <button><Link to='/doctorHome'>Are You Doctor? Click Here</Link></button>
+        </div> */}
+        
+
+        <div className="patient__section">
+          <button><Link to='/patientHome' className='link'>Patient Login</Link></button>
+        </div>
+        <div className="doctor__section">
+          <button><Link to ='/doctorHome' className='link'>Doctor Login</Link></button>
+        </div>
+        <div className="admin__section">
+          <button><Link to='/adminHome' className='link'>Admin Login</Link></button>
         </div>
     </div>
   )
