87fce03889c476c5bc4ea99e8c51cb79b05e383e
Aakash Kumar Chaudhary <aakaas.c.4508@gmail.com>
2023-07-27 09:54:10 +0530

    modified features page

--- client/src/patientScreen/PatientFeatures.jsx
+++ client/src/patientScreen/PatientFeatures.jsx
@@ -6,6 +6,7 @@ import image3 from '../patientComponentsAssets/image3.jpg'
 import image4 from '../patientComponentsAssets/image4.jpg'
 import image5 from '../patientComponentsAssets/image5.jpg'
 import image6 from '../patientComponentsAssets/image6.jpg'
+import {BiSolidOffer} from 'react-icons/bi';
 const PatientFeatures = () => {
   return (
     <div className="body">
@@ -13,45 +14,31 @@ const PatientFeatures = () => {
             <div className="all_cards">
                 <div className="card">
                     <img src={consultant} alt="" />
-                    <div className="offer">
-                        <h6>40% off</h6>
-                    </div>
-                    <h3>Get your consultant TODAY</h3>
+                    
+                    <p><b>One Change Group’s therapists are prepared to guide you on your journey to healthy change. We offer licensed, secure, confidential, online HIPAA compliant counseling. We are currently accepting new clients in Oregon and Idaho and would be happy to explore your needs</b>.</p>
                 </div>
                 <div className="card">
                     <img src={image2} alt="" />
-                    <div className="offer">
-                        <h6>40% off</h6>
-                    </div>
-                    <h3>Get your consultant TODAY</h3>
+                    
+                    <p><b>Flat 30% off on your first appointment. Hurry!</b></p>
                 </div>
                 <div className="card">
                     <img src={image3} alt="" />
-                    <div className="offer">
-                        <h6>40% off</h6>
-                    </div>
-                    <h3>Get your consultant TODAY</h3>
+                    
+                    <p><b>Comprehensive health check-ups for early detection and personalized care</b></p>
                 </div>
                 <div className="card">
                     <img src={image4} alt="" />
-                    <div className="offer">
-                        <h6>40% off</h6>
-                    </div>
-                    <h3>Get your consultant TODAY</h3>
+                    
+                    <p><b>Providing skilled and compassionate surgical care for your health needs</b></p>
                 </div>
                 <div className="card">
                     <img src={image5} alt="" />
-                    <div className="offer">
-                        <h6>40% off</h6>
-                    </div>
-                    <h3>Get your consultant TODAY</h3>
+                    <p><b>One Change Group’s therapists are prepared to guide you on your journey to healthy change. We offer licensed, secure, confidential, online HIPAA compliant counseling. We are currently accepting new clients in Oregon and Idaho and would be happy to explore your needs</b>.</p>
                 </div>
                 <div className="card">
                     <img src={image6} alt="" />
-                    <div className="offer">
-                        <h6>40% off</h6>
-                    </div>
-                    <h3>Get your consultant TODAY</h3>
+                    <p><b>One Change Group’s therapists are prepared to guide you on your journey to healthy change. We offer licensed, secure, confidential, online HIPAA compliant counseling. We are currently accepting new clients in Oregon and Idaho and would be happy to explore your needs</b>.</p>
                 </div>
             </div>
         </div>
